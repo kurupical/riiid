5cb718674a79d2fc52f85596789926672a8dc388
Unknown <kurupical@gmail.com>
2020-10-13 19:05:04 +0900

    ex003_feature_engineering

--- feature_engineering/user_id.py
+++ feature_engineering/user_id.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import numpy as np
 from typing import List
 from logging import Logger
 
+def user_intelligency(df):
+    def f(series):
+        return series.shift(1).cumsum() / (np.arange(len(series)) + 1)
+
+    df["intelligency_point"] = df["answered_correctly"] / (df["target_enc_content_id"]+0.1)
+    df["intelligency_point_cummean"] = df.groupby("user_id")["intelligency_point"].transform(f).astype("float16")
+
+    df = df.drop("intelligency_point", axis=1)
+    return df
---
+++ pipeline/p_003_targetencoding.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from feature_engineering.all_id import one_hot_encoding_count, target_encoding
+from feature_engineering.user_id import user_intelligency
+
+def transform(df: pd.DataFrame):
+    """
+    question, lectureがマージされたdfを加工する.
+    :param df:
+    :return:
+    """
+
+    # ---------------
+    # 前処理
+    # ---------------
+    df["not_answered"] = (df["user_answer"] == -1).astype(int)
+
+    # ---------------
+    # row_count
+    # ---------------
+    for id_name in ["user_id"]:
+        df[f"{id_name}_count"] = df.groupby(id_name).cumcount()
+
+    df[f"user_content_id_count"] = df.groupby(["user_id", "content_id"]).cumcount()
+
+    # ---------------
+    # one_hot_encoding_count
+    # ---------------
+    for id_name in ["user_id"]:
+        for col in ["answered_correctly", "not_answered"]:
+            print(f"target_encoding {col}")
+            df = one_hot_encoding_count(df=df,
+                                        id_name=id_name,
+                                        column=col)
+    for col in [["user_id"], ["content_id"], ["task_container_id"]]:
+        print(f"target_encoding {col}")
+        df = target_encoding(df=df, cols=col)
+
+    print("user_intelligency")
+    df = user_intelligency(df)
+    df = df.drop("not_answered", axis=1)
+    df["prior_question_had_explanation"] = df["prior_question_had_explanation"].fillna(-1).astype("int8")
+    return df
