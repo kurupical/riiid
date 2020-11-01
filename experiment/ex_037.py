07042665b2d1e79183854e456a2a8c8759d8fec2
Unknown <kurupical@gmail.com>
2020-11-01 22:40:18 +0900

    exp035-038

---
+++ experiment/ex_037.py
@@ -0,0 +1,134 @@
+from feature_engineering.feature_factory import \
+    FeatureFactoryManager, \
+    TargetEncoder, \
+    CountEncoder, \
+    MeanAggregator, \
+    NUniqueEncoder, \
+    TagsSeparator2, \
+    ShiftDiffEncoder, \
+    UserLevelEncoder2, \
+    TargetEncodeVsUserId, \
+    Counter, \
+    PreviousAnswer, \
+    PartSeparator
+from experiment.common import get_logger
+import pandas as pd
+from model.lgbm import train_lgbm_cv
+from sklearn.model_selection import KFold
+from datetime import datetime as dt
+import numpy as np
+import os
+import glob
+import time
+import tqdm
+
+output_dir = f"../output/{os.path.basename(__file__).replace('.py', '')}/{dt.now().strftime('%Y%m%d%H%M%S')}/"
+
+for fname in glob.glob("../input/riiid-test-answer-prediction/split10/*"):
+    print(fname)
+    df = pd.read_pickle(fname).sort_values(["user_id", "timestamp"])
+    # df = pd.concat([pd.read_pickle(fname).head(500), pd.read_pickle(fname).tail(500)])
+    df["answered_correctly"] = df["answered_correctly"].replace(-1, np.nan)
+    df["prior_question_had_explanation"] = df["prior_question_had_explanation"].fillna(-1).astype("int8")
+    logger = get_logger()
+    feature_factory_dict = {}
+    feature_factory_dict["tags"] = {
+        "TagsSeparator": TagsSeparator2()
+    }
+    # tags = [f"tag_{x}" for x in np.arange(1, 188)]
+    tags = [f"tag_{x}" for x in np.arange(1, 50)]
+
+    for tag in tags:
+        column = ("user_id", tag)
+        feature_factory_dict[column] = {
+            "CountEncoder": CountEncoder(column=list(column)),
+            "TargetEncoder": TargetEncoder(column=list(column))
+        }
+
+    for column in ["part", "content_id", "content_type_id", "user_id", "prior_question_had_explanation",
+                   ("user_id", "prior_question_had_explanation"), ("user_id", "part"),
+                   ("prior_question_had_explanation", "correct_answer"),
+                   ("prior_question_had_explanation", "part"),
+                   ("user_id", "part1"), ("user_id", "part2"), ("user_id", "part3"), ("user_id", "part4"),
+                   ("user_id", "part5"), ("user_id", "part6"), ("user_id", "part7")]:
+        is_partial_fit = (column == "content_id")
+
+        if type(column) == str:
+            feature_factory_dict[column] = {
+                "CountEncoder": CountEncoder(column=column),
+                "TargetEncoder": TargetEncoder(column=column, is_partial_fit=is_partial_fit)
+            }
+        else:
+            feature_factory_dict[column] = {
+                "CountEncoder": CountEncoder(column=list(column)),
+                "TargetEncoder": TargetEncoder(column=list(column), is_partial_fit=is_partial_fit)
+            }
+    feature_factory_dict["part"]["PartSeparator"] = PartSeparator()
+
+    feature_factory_dict["user_id"]["ShiftDiffEncoderTimestamp"] = ShiftDiffEncoder(groupby="user_id",
+                                                                                    column="timestamp",
+                                                                                    is_partial_fit=True)
+    feature_factory_dict["user_id"]["ShiftDiffEncoderContentId"] = ShiftDiffEncoder(groupby="user_id",
+                                                                                    column="content_id")
+    feature_factory_dict["user_id"]["MeanAggregatorTimestamp"] = MeanAggregator(column="user_id",
+                                                                                agg_column="timestamp",
+                                                                                remove_now=False)
+    for column in ["user_id", "content_id"]:
+        feature_factory_dict[column][f"MeanAggregatorPriorQuestionElapsedTimeby{column}"] = MeanAggregator(column=column,
+                                                                                                           agg_column="prior_question_elapsed_time",
+                                                                                                           remove_now=True)
+        feature_factory_dict["user_id"][f"MeanAggregatorPriorQuestionshiftdiffbyuser_id"] = MeanAggregator(column=column,
+                                                                                                           agg_column="shiftdiff_timestamp_by_user_id",
+                                                                                                           remove_now=False)
+    for column in ["part1", "part2", "part3", "part4", "part5", "part6", "part7"]:
+        feature_factory_dict[column] = {}
+        feature_factory_dict[column][f"MeanAggregatorPriorQuestionElapsedTimeby{column}"] = MeanAggregator(column=column,
+                                                                                                           agg_column="prior_question_elapsed_time",
+                                                                                                           remove_now=True)
+        feature_factory_dict[column][f"MeanAggregatorPriorQuestionshiftdiffby{column}"] = MeanAggregator(column=column,
+                                                                                                         agg_column="shiftdiff_timestamp_by_user_id",
+                                                                                                         remove_now=False)
+
+    feature_factory_dict["user_id"]["UserLevelEncoder2ContentId"] = UserLevelEncoder2(vs_column="content_id")
+
+    feature_factory_manager = FeatureFactoryManager(feature_factory_dict=feature_factory_dict,
+                                                    logger=logger,
+                                                    split_num=10)
+    df = feature_factory_manager.all_predict(df)
+    os.makedirs(output_dir, exist_ok=True)
+    params = {
+        'objective': 'binary',
+        'num_leaves': 32,
+        'min_data_in_leaf': 15,  # 42,
+        'max_depth': -1,
+        'learning_rate': 0.3,
+        'boosting': 'gbdt',
+        'bagging_fraction': 0.7,  # 0.5,
+        'feature_fraction': 0.9,
+        'bagging_seed': 0,
+        'reg_alpha': 5,  # 1.728910519108444,
+        'reg_lambda': 5,
+        'random_state': 0,
+        'metric': 'auc',
+        'verbosity': -1,
+        "n_estimators": 10000,
+        "early_stopping_rounds": 100
+    }
+    df.tail(1000).to_csv("exp028.csv", index=False)
+
+    del_col = [f"tag_{x}" for x in np.arange(1, 188)] + [f"count_enc_['user_id', 'tag_{x}']" for x in np.arange(1, 50)]
+    print(df.shape)
+    df = df.drop(del_col, axis=1)
+    df = df.drop(["user_answer", "tags", "type_of"], axis=1)
+    df = df[df["answered_correctly"].notnull()]
+    print(df.columns)
+    print(df.shape)
+
+    model_id = os.path.basename(fname).replace(".pickle", "")
+    print(model_id)
+    train_lgbm_cv(df,
+                  params=params,
+                  output_dir=output_dir,
+                  model_id=model_id,
+                  exp_name=model_id,
+                  drop_user_id=True)
