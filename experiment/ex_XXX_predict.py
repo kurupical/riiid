ce63c0da552d9b7a226939e534cde35d5b5eb893
Unknown <kurupical@gmail.com>
2020-11-15 10:54:43 +0900

    高速化２

--- experiment/ex_XXX_predict.py
+++ experiment/ex_XXX_predict.py
@@ -89,16 +89,16 @@ def run(debug,
     feature_factory_manager.logger = logger
 
     iter_test = env.iter_test()
-    df_test_prev = pd.DataFrame()
+    df_test_prev = []
+    df_test_prev_rows = 0
     answered_correctlies = []
     user_answers = []
     i = 0
-    t = time.time()
     for (df_test, df_sample_prediction) in iter_test:
         i += 1
-        logger.info(f"[time: {int(time.time() - t)}iteration {i}: data_length: {len(df_test)}")
+        logger.info(f"[iteration {i}: data_length: {len(df_test)}")
         # 前回のデータ更新
-        if len(df_test_prev) > 0: # 初回のみパスするためのif
+        if df_test_prev_rows > 0: # 初回のみパスするためのif
             answered_correctly = df_test.iloc[0]["prior_group_answers_correct"]
             user_answer = df_test.iloc[0]["prior_group_responses"]
             answered_correctlies.extend([int(x) for x in answered_correctly.replace("[", "").replace("'", "").replace("]", "").replace(" ", "").split(",")])
@@ -106,8 +106,10 @@ def run(debug,
 
         if debug:
             update_record = 1
-        if len(df_test_prev) > update_record:
-            logger.info("fitting...")
+        if df_test_prev_rows > update_record:
+            logger.info("------ fitting ------")
+            logger.info("concat df")
+            df_test_prev = pd.concat(df_test_prev)
             df_test_prev["answered_correctly"] = answered_correctlies
             df_test_prev["user_answer"] = user_answers
             # df_test_prev = df_test_prev.drop(prior_columns, axis=1)
@@ -115,15 +117,17 @@ def run(debug,
             df_test_prev["answered_correctly"] = df_test_prev["answered_correctly"].replace(-1, np.nan)
             df_test_prev["prior_question_had_explanation"] = df_test_prev["prior_question_had_explanation"].fillna(-1).astype("int8")
 
+            logger.info("fit data")
             feature_factory_manager.fit(df_test_prev)
 
-            df_test_prev = pd.DataFrame()
+            df_test_prev = []
+            df_test_prev_rows = 0
             answered_correctlies = []
             user_answers = []
         # 今回のデータ取得&計算
 
         # logger.info(f"[time: {int(time.time() - t)}dataload")
-        logger.info(f"merge... ")
+        logger.info(f"------ question&lecture merge ------")
         w_df1 = pd.merge(df_test[df_test["content_type_id"] == 0], df_question, how="left", left_on="content_id",
                          right_on="question_id")
         w_df2 = pd.merge(df_test[df_test["content_type_id"] == 1], df_lecture, how="left", left_on="content_id",
@@ -133,12 +137,12 @@ def run(debug,
         df_test["correct_answer"] = df_test["correct_answer"].fillna(-1)
         df_test["bundle_id"] = df_test["bundle_id"].fillna(-1)
 
-        logger.info(f"transform... ")
+        logger.info(f"------ transform ------ ")
         df_test["prior_question_had_explanation"] = df_test["prior_question_had_explanation"].astype("float16").fillna(-1).astype("int8")
 
         df = feature_factory_manager.partial_predict(df_test)
         df.columns = [x.replace(" ", "_") for x in df.columns]
-        logger.info(f"predict...")
+        logger.info(f"------ predict ------")
 
         # predict
         predicts = []
@@ -147,13 +151,14 @@ def run(debug,
         for model in models:
             predicts.append(model.predict(w_df))
 
-        logger.info("other...")
+        logger.info("------ other ------")
         df["answered_correctly"] = np.array(predicts).transpose().mean(axis=1)
         df_sample_prediction = df[df["content_type_id"] == 0][["row_id", "answered_correctly"]]
         env.predict(df_sample_prediction)
-        df_test_prev = df_test_prev.append(df[cols + ["user_id", "tags"]])
+        df_test_prev.append(df[cols + ["user_id", "tags"]])
+        df_test_prev_rows += len(df)
         if i < 5:
-            df_test_prev.to_csv(f"{i}.csv")
+            df.to_csv(f"{i}.csv")
 
 if __name__ == "__main__":
     run(debug=True,
