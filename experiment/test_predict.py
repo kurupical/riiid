dd88bab85b1fe713f8bbd63b93d5458b72e7ab7b
Unknown <kurupical@gmail.com>
2020-11-18 00:15:41 +0900

    いろいろ

--- experiment/test_predict.py
+++ experiment/test_predict.py
@@ -8,6 +8,7 @@ from feature_engineering.feature_factory import \
     UserLevelEncoder2, \
     NUniqueEncoder, \
     ShiftDiffEncoder, \
+    TargetEncodeVsUserId, \
     PartSeparator, \
     UserCountBinningEncoder, \
     CategoryLevelEncoder, \
@@ -26,7 +27,7 @@ import time
 import warnings
 warnings.filterwarnings("ignore")
 
-model_dir = "../output/ex_084/20201116212648"
+model_dir = "../output/ex_073/20201114084317"
 
 data_types_dict = {
     'row_id': 'int64',
@@ -93,24 +94,31 @@ def run(debug,
     answered_correctlies = []
     user_answers = []
     i = 0
-    for (df_test, df_sample_prediction) in iter_test:
+
+    df_all = pd.read_pickle(f"{os.path.dirname(files_dir)}/train_0.pickle").head(1000000)\
+        #.drop(
+        #["question_id", "bundle_id", "correct_answer", "part", "lecture_id", "tag", "part", "type_of"], axis=1
+        #)
+    for idx in range(len(df_all)//20):
+        df_test = df_all.iloc[i*20:(i+1)*20]
         i += 1
         logger.info(f"[iteration {i}: data_length: {len(df_test)}")
         # 前回のデータ更新
+        """
         if df_test_prev_rows > 0: # 初回のみパスするためのif
             answered_correctly = df_test.iloc[0]["prior_group_answers_correct"]
             user_answer = df_test.iloc[0]["prior_group_responses"]
             answered_correctlies.extend([int(x) for x in answered_correctly.replace("[", "").replace("'", "").replace("]", "").replace(" ", "").split(",")])
             user_answers.extend([int(x) for x in user_answer.replace("[", "").replace("'", "").replace("]", "").replace(" ", "").split(",")])
-
+        """
         if debug:
             update_record = 1
         if df_test_prev_rows > update_record:
             logger.info("------ fitting ------")
             logger.info("concat df")
             df_test_prev = pd.concat(df_test_prev)
-            df_test_prev["answered_correctly"] = answered_correctlies
-            df_test_prev["user_answer"] = user_answers
+            # df_test_prev["answered_correctly"] = answered_correctlies
+            # df_test_prev["user_answer"] = user_answers
             # df_test_prev = df_test_prev.drop(prior_columns, axis=1)
             df_test_prev = df_test_prev[df_test_prev["answered_correctly"] != -1]
             df_test_prev["answered_correctly"] = df_test_prev["answered_correctly"].replace(-1, np.nan)
@@ -135,7 +143,6 @@ def run(debug,
         df_test["tag"] = df_test["tag"].fillna(-1)
         df_test["correct_answer"] = df_test["correct_answer"].fillna(-1)
         df_test["bundle_id"] = df_test["bundle_id"].fillna(-1)
-
         logger.info(f"------ transform ------ ")
         df_test["prior_question_had_explanation"] = df_test["prior_question_had_explanation"].astype("float16").fillna(-1).astype("int8")
 
@@ -152,9 +159,8 @@ def run(debug,
 
         logger.info("------ other ------")
         df["answered_correctly"] = np.array(predicts).transpose().mean(axis=1)
-        df_sample_prediction = df[df["content_type_id"] == 0][["row_id", "answered_correctly"]]
-        env.predict(df_sample_prediction)
-        df_test_prev.append(df[cols + ["user_id", "tags"]])
+        df_sample_prediction = df[df["content_type_id"] == 0][["answered_correctly"]]
+        df_test_prev.append(df[cols + ["user_id", "tags", "answered_correctly"]])
         df_test_prev_rows += len(df)
         if i < 5:
             df.to_csv(f"{i}.csv")
